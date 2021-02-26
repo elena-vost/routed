7b85c363aee502e8faea003b79293abfbf923adb
elena-vost <angara1298@gmail.com>
2021-02-26 15:01:05 +0100

    route

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Route } from "react-router-dom";
 import About from "./About";
 import Dog from "./Dog";
 import Contact from "./Contact";
@@ -6,7 +7,11 @@ import "./App.css";
 
 class App extends Component {
   render() {
-    return <div className="App"></div>;
+    return (
+      <div className="App">
+        <Route path="/dog" component={Dog} />
+      </div>
+    );
   }
 }
 export default App;
