21d6db38071cf4ce17c7386dc5e10064b2323f8d
elena-vost <angara1298@gmail.com>
2021-02-26 15:28:12 +0100

    navlinks imported1

--- src/App.js
+++ src/App.js
@@ -10,14 +10,32 @@ class App extends Component {
     return (
       <div className="App">
         <nav className="App-nav">
-          <NavLink to="/dog">Dog</Link>
-          <NavLink to="/contact">Contact</Link>
-          <NavLink to="/">About</Link>
+          <NavLink to="/dog">Dog</NavLink>
+          <NavLink to="/contact">Contact</NavLink>
+          <NavLink to="/">About</NavLink>
         </nav>
         <Switch>
-          <Route exact path="/" component={About} />
-          <Route exact path="/dog" component={Dog} />
-          <Route exact path="/contact" component={Contact} />
+          <Route
+            exact
+            activeClassName="active-link"
+            exact
+            path="/"
+            component={About}
+          />
+          <Route
+            exact
+            activeClassName="active-link"
+            exact
+            path="/dog"
+            component={Dog}
+          />
+          <Route
+            exact
+            activeClassName="active-link"
+            exact
+            path="/contact"
+            component={Contact}
+          />
         </Switch>
       </div>
     );
