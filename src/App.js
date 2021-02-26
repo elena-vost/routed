bce5803887004fabc8efdd573922c0f205ca0165
elena-vost <angara1298@gmail.com>
2021-02-26 15:12:47 +0100

    switch impoted

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import About from "./About";
 import Dog from "./Dog";
 import Contact from "./Contact";
@@ -9,7 +9,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Route path="/dog" component={Dog} />
+        <Switch>
+          <Route exact path="/" component={About} />
+          <Route exact path="/dog" component={Dog} />
+          <Route exact path="/contact" component={Contact} />
+        </Switch>
       </div>
     );
   }
